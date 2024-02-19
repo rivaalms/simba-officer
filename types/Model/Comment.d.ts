2e16411ff75cffe6bc2431d4ba5df65b8ccf3f5d
rivaalms <rvalms@icloud.com>
2024-02-19 21:11:23 +0700

    chore: type definitions

---
+++ types/Model/Comment.d.ts
@@ -0,0 +1,13 @@
+declare namespace Model {
+   type Comment = {
+      id: number
+      user_id: number
+      data_id: number
+      reply_to: number | null
+      message: string
+      created_at?: string | null
+      updated_at?: string | null
+      user?: Model.User<School | Supervisor | Officer | null>
+      replies?: Model.Comment[]
+   }
+}
